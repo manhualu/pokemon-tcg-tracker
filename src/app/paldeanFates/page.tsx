b9136aefc3d8dab2153557472dba0f9a4ef2a443
Manhua Lu <manhua.lu@student.unsw.edu.au>
2024-12-18 18:41:04 +1100

    set up pages

---
+++ src/app/paldeanFates/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const paldeanFates = () => {
+  return (
+    <>
+      <div className="">Set: Paldean Fates</div>
+      <div>Cards:</div>
+    </>
+  );
+};
+
+export default paldeanFates;
